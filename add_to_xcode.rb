208a2a8f31e5fd0127c2e28fda954f2a7a815631
rotnitxe <maticsaldias@gmail.com>
2026-07-16 21:11:40 -0400

    fix(ios): correct absolute paths for new swift files in pbxproj using SOURCE_ROOT

--- add_to_xcode.rb
+++ add_to_xcode.rb
@@ -12,20 +12,23 @@ def add_file_to_project(project, target, file_name, group_path)
   group_names.each do |name|
     next_group = current_group.groups.find { |g| g.display_name == name || g.path == name }
     if next_group.nil?
-      next_group = current_group.new_group(name, name) # The second argument sets the path
+      next_group = current_group.new_group(name, name)
     end
     current_group = next_group
   end
   
-  old_ref = current_group.files.find { |f| f.display_name == file_name || f.path == file_name }
+  old_ref = current_group.files.find { |f| f.display_name == file_name || f.path == file_name || f.name == file_name }
   if old_ref
     target.source_build_phase.remove_file_reference(old_ref)
     old_ref.remove_from_project
   end
   
   file_ref = current_group.new_file(file_name)
+  file_ref.set_path("#{group_path}/#{file_name}")
+  file_ref.source_tree = 'SOURCE_ROOT'
+  
   target.source_build_phase.add_file_reference(file_ref)
-  puts "Added #{file_name} correctly"
+  puts "Added #{file_name} correctly with absolute project path"
 end
 
 add_file_to_project(project, target, 'AppColors.swift', 'KPKNFit/Presentation/Theme')
